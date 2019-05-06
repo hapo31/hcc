d7b7adaf2d09a8bfb36b9c17dde199d6e2ec2285
happou31 <happo31@gmail.com>
2019-05-07 01:02:06 +0900

    add: すごい長い名前の変数

--- test/test.sh
+++ test/test.sh
@@ -78,6 +78,7 @@ try 42 'n = 0; while (n < 42) n = n + 1; return n;'
 try 0 'n = 42; while (n > 0) n = n - 1; return n;'
 try 45 'result = 0; for (n = 0; n < 10; n = n + 1) result = result + n; return result;'
 try 120 '(((((((((((((((1 + 2) + 3) + 4) + 5) + 6) + 7) + 8) + 9) + 10) + 11) + 12) + 13) + 14) + 15));'
+try 10 'super_long_var_name_wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww = 10; return super_long_var_name_wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww;'
 
 
 echo ---------------------------------------------------------------------
