0213c2b7ccd2fcf7e3271888584f419f4f03eff6
happou31 <happo31@gmail.com>
2019-05-07 21:21:42 +0900

    fix: コード生成関数を共通化した

--- src/codegen.c
+++ src/codegen.c
@@ -1,7 +1,16 @@
 #include "codegen.h"
 
 #include <stdio.h>
+#include <stdarg.h>
 
+void emit(const char *fmt, ...);
+void initial(FILE *fp);
+void prologue(FILE *fp);
+void gen(Node *node);
+void gen_lvalue(Node *node);
+void epilogue(FILE *fp);
+
+FILE *output_fp;
 Map *identifiers;
 Vector *code;
 int if_count = 0;
@@ -9,23 +18,31 @@ int else_count = 0;
 int while_count = 0;
 int for_count = 0;
 
+void emit(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(output_fp, fmt, ap);
+    fprintf(output_fp, "\n");
+}
+
 void initial(FILE *fp)
 {
-    fprintf(fp, ".intel_syntax noprefix\n");
-    fprintf(fp, ".global main\n");
-    fprintf(fp, "main: \n");
+    emit(".intel_syntax noprefix\n");
+    emit(".global main\n");
+    emit("main: \n");
 }
 
 void prologue(FILE *fp)
 {
     // プロローグ
     // リターンアドレスをスタックに push し、ベースポインタの指すアドレスをスタックの先頭が指すアドレスとする
-    fprintf(fp, "    push rbp\n");
-    fprintf(fp, "    mov rbp, rsp\n");
-    fprintf(fp, "    sub rsp, %ld\n", identifiers->len * VAR_SIZE); // 変数はすべてVAR_SIZEとしておく
+    emit("    push rbp\n");
+    emit("    mov rbp, rsp\n");
+    emit("    sub rsp, %ld\n", identifiers->len * VAR_SIZE); // 変数はすべてVAR_SIZEとしておく
 }
 
-void gen_lvalue(FILE *fp, Node *node)
+void gen_lvalue(Node *node)
 {
     if (node->type != ND_IDENT)
     {
@@ -33,34 +50,34 @@ void gen_lvalue(FILE *fp, Node *node)
     }
     int ident_index = (int)read_map(identifiers, node->name);
     int offset = ident_index * VAR_SIZE;
-    fprintf(fp, "    mov rax, rbp\n");
-    fprintf(fp, "    sub rax, %d\n", offset);
-    fprintf(fp, "    push rax\n");
+    emit("    mov rax, rbp\n");
+    emit("    sub rax, %d\n", offset);
+    emit("    push rax\n");
 }
 
-void gen(FILE *fp, Node *node)
+void gen(Node *node)
 {
     if (node->type == ND_IF)
     {
-        gen(fp, node->condition);
-        fprintf(fp, "    pop rax\n");
-        fprintf(fp, "    cmp rax, 0\n");
+        gen(node->condition);
+        emit("    pop rax\n");
+        emit("    cmp rax, 0\n");
         // else 節があるかどうか
         if (node->else_ != NULL)
         {
-            fprintf(fp, "    je .Lelse%d\n", else_count);
-            gen(fp, node->then);
-            fprintf(fp, "    jmp .Lendif%d\n", if_count);
-            fprintf(fp, ".Lelse%d:\n", else_count);
-            gen(fp, node->else_);
-            fprintf(fp, ".Lendif%d:\n", if_count);
+            emit("    je .Lelse%d\n", else_count);
+            gen(node->then);
+            emit("    jmp .Lendif%d\n", if_count);
+            emit(".Lelse%d:\n", else_count);
+            gen(node->else_);
+            emit(".Lendif%d:\n", if_count);
             ++else_count;
         }
         else
         {
-            fprintf(fp, "    je .Lendif%d\n", if_count);
-            gen(fp, node->then);
-            fprintf(fp, ".Lendif%d:\n", if_count);
+            emit("    je .Lendif%d\n", if_count);
+            gen(node->then);
+            emit(".Lendif%d:\n", if_count);
         }
 
         ++if_count;
@@ -69,30 +86,30 @@ void gen(FILE *fp, Node *node)
 
     if (node->type == ND_WHILE)
     {
-        fprintf(fp, ".Lwhile%d:\n", while_count);
-        gen(fp, node->condition);
-        fprintf(fp, "    pop rax\n");
-        fprintf(fp, "    cmp rax, 0\n");
-        fprintf(fp, "    je .Lendwhile%d\n", while_count);
-        gen(fp, node->then);
-        fprintf(fp, "    jmp .Lwhile%d\n", while_count);
-        fprintf(fp, ".Lendwhile%d:\n", while_count);
+        emit(".Lwhile%d:\n", while_count);
+        gen(node->condition);
+        emit("    pop rax\n");
+        emit("    cmp rax, 0\n");
+        emit("    je .Lendwhile%d\n", while_count);
+        gen(node->then);
+        emit("    jmp .Lwhile%d\n", while_count);
+        emit(".Lendwhile%d:\n", while_count);
         ++while_count;
         return;
     }
 
     if (node->type == ND_FOR)
     {
-        gen(fp, node->init_expression);
-        fprintf(fp, ".Lfor%d:\n", for_count);
-        gen(fp, node->condition);
-        fprintf(fp, "    pop rax\n");
-        fprintf(fp, "    cmp rax, 0\n");
-        fprintf(fp, "    je .Lforend%d\n", for_count);
-        gen(fp, node->then);
-        gen(fp, node->loop_expression);
-        fprintf(fp, "    jmp .Lfor%d\n", for_count);
-        fprintf(fp, ".Lforend%d:\n", for_count);
+        gen(node->init_expression);
+        emit(".Lfor%d:\n", for_count);
+        gen(node->condition);
+        emit("    pop rax\n");
+        emit("    cmp rax, 0\n");
+        emit("    je .Lforend%d\n", for_count);
+        gen(node->then);
+        gen(node->loop_expression);
+        emit("    jmp .Lfor%d\n", for_count);
+        emit(".Lforend%d:\n", for_count);
 
         ++for_count;
         return;
@@ -100,106 +117,107 @@ void gen(FILE *fp, Node *node)
 
     if (node->type == ND_RETURN)
     {
-        gen(fp, node->lhs);
-        fprintf(fp, "    pop rax\n");
-        fprintf(fp, "    mov rsp, rbp\n");
-        fprintf(fp, "    pop rbp\n");
-        fprintf(fp, "    ret\n");
+        gen(node->lhs);
+        emit("    pop rax\n");
+        emit("    mov rsp, rbp\n");
+        emit("    pop rbp\n");
+        emit("    ret\n");
         return;
     }
 
     if (node->type == ND_NUM)
     {
-        fprintf(fp, "    push %d\n", node->value);
+        emit("    push %d\n", node->value);
         return;
     }
 
     if (node->type == ND_IDENT)
     {
-        gen_lvalue(fp, node);
-        fprintf(fp, "    pop rax\n");
-        fprintf(fp, "    mov rax, [rax]\n");
-        fprintf(fp, "    push rax\n");
+        gen_lvalue(node);
+        emit("    pop rax\n");
+        emit("    mov rax, [rax]\n");
+        emit("    push rax\n");
         return;
     }
 
     if (node->type == '=')
     {
-        gen_lvalue(fp, node->lhs);
-        gen(fp, node->rhs);
+        gen_lvalue(node->lhs);
+        gen(node->rhs);
 
-        fprintf(fp, "    pop rdi\n");
-        fprintf(fp, "    pop rax\n");
-        fprintf(fp, "    mov [rax], rdi\n");
-        fprintf(fp, "    push rdi\n");
+        emit("    pop rdi\n");
+        emit("    pop rax\n");
+        emit("    mov [rax], rdi\n");
+        emit("    push rdi\n");
         return;
     }
 
-    gen(fp, node->lhs);
-    gen(fp, node->rhs);
+    gen(node->lhs);
+    gen(node->rhs);
 
-    fprintf(fp, "    pop rdi\n");
-    fprintf(fp, "    pop rax\n");
+    emit("    pop rdi\n");
+    emit("    pop rax\n");
 
     switch (node->type)
     {
     case '+':
-        fprintf(fp, "    add rax, rdi\n");
+        emit("    add rax, rdi\n");
         break;
     case '-':
-        fprintf(fp, "    sub rax, rdi\n");
+        emit("    sub rax, rdi\n");
         break;
     case '*':
-        fprintf(fp, "    mul rdi\n");
+        emit("    mul rdi\n");
         break;
     case '/':
-        fprintf(fp, "    mov rdx, 0\n");
-        fprintf(fp, "    div rdi\n");
+        emit("    mov rdx, 0\n");
+        emit("    div rdi\n");
         break;
     case '%':
-        fprintf(fp, "    mov rdx, 0\n");
-        fprintf(fp, "    div rdi\n");
-        fprintf(fp, "    mov rax, rdx\n");
+        emit("    mov rdx, 0\n");
+        emit("    div rdi\n");
+        emit("    mov rax, rdx\n");
         break;
     case ND_EQ:
-        fprintf(fp, "    cmp rax, rdi\n");
-        fprintf(fp, "    sete al\n");
-        fprintf(fp, "    movzb rax, al\n");
+        emit("    cmp rax, rdi\n");
+        emit("    sete al\n");
+        emit("    movzb rax, al\n");
         break;
     case ND_NE:
-        fprintf(fp, "    cmp rax, rdi\n");
-        fprintf(fp, "    setne al\n");
-        fprintf(fp, "    movzb rax, al\n");
+        emit("    cmp rax, rdi\n");
+        emit("    setne al\n");
+        emit("    movzb rax, al\n");
         break;
     case ND_LE:
-        fprintf(fp, "    cmp rax, rdi\n");
-        fprintf(fp, "    setle al\n");
-        fprintf(fp, "    movzb rax, al\n");
+        emit("    cmp rax, rdi\n");
+        emit("    setle al\n");
+        emit("    movzb rax, al\n");
         break;
     case ND_LT:
-        fprintf(fp, "    cmp rax, rdi\n");
-        fprintf(fp, "    setl al\n");
-        fprintf(fp, "    movzb rax, al\n");
+        emit("    cmp rax, rdi\n");
+        emit("    setl al\n");
+        emit("    movzb rax, al\n");
         break;
     default:
         break;
     }
 
-    fprintf(fp, "    push rax\n");
+    emit("    push rax\n");
 }
 
 void epilogue(FILE *fp)
 {
     // エピローグ
 
-    printf("    mov rsp, rbp\n");
-    printf("    pop rbp\n");
+    emit("    mov rsp, rbp\n");
+    emit("    pop rbp\n");
     // rax に演算結果が残っているので、それがこのプログラムの出力になる
-    printf("    ret\n");
+    emit("    ret\n");
 }
 
 void codegen(FILE *fp, ParseResult *parse_result)
 {
+    output_fp = fp;
     code = parse_result->code;
     identifiers = parse_result->identifiers;
     initial(fp);
@@ -209,10 +227,10 @@ void codegen(FILE *fp, ParseResult *parse_result)
     for (int i = 0; i < code->len; ++i)
     {
         // 抽象構文木からアセンブラを生成
-        gen(fp, code->data[i]);
+        gen(code->data[i]);
 
         // スタックに式の評価結果が乗っているので pop しておく
-        fprintf(fp, "    pop rax\n");
+        emit("    pop rax\n");
     }
 
     epilogue(fp);
--- src/codegen.h
+++ src/codegen.h
@@ -7,9 +7,4 @@
 #include "map.h"
 #include "vector.h"
 
-void initial(FILE *fp);
-void prologue(FILE *fp);
-void gen(FILE *fp, Node *node);
-void gen_lvalue(FILE *fp, Node *node);
-void epilogue(FILE *fp);
 void codegen(FILE *target, ParseResult *parse_result);
