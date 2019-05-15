2ff5c8c248aa1ec24c25b478e41e538bb37e8a08
happou31 <happo31@gmail.com>
2019-05-16 02:35:03 +0900

    add: 文字列型

---
+++ src/str.h
@@ -0,0 +1,12 @@
+#include <stddef.h>
+
+typedef struct
+{
+  size_t len;
+  size_t capacity;
+  char *str;
+} String;
+
+String *new_string();
+String *append_string(String *src, const char *str);
+String *put_string(String *src, const char *str);
