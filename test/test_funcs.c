e46bc0cb8a8d718e9f8a707ab1c781f8afeb4243
happou31 <happo31@gmail.com>
2019-05-25 00:50:43 +0900

    fix: indent

--- test/test_funcs.c
+++ test/test_funcs.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int f()
 {
-  printf("test!!!\n");
+    printf("test!!!\n");
 }
 
 int g(int i, int j, int k, int l, int m, int n)
 {
-  printf("test: %d, %d, %d, %d, %d, %d\n", i, j, k, l, m, n);
+    printf("test: %d, %d, %d, %d, %d, %d\n", i, j, k, l, m, n);
 
-  return i + j + k + l + m + n;
+    return i + j + k + l + m + n;
 }
 
 int g2(int i, int j, int k, int l, int m, int n, int o)
 {
-  printf("test: %d, %d, %d, %d, %d, %d, %d\n", i, j, k, l, m, n, o);
+    printf("test: %d, %d, %d, %d, %d, %d, %d\n", i, j, k, l, m, n, o);
 
-  return i + j + k + l + m + n + o;
+    return i + j + k + l + m + n + o;
 }
 
 int f1(int i, int j, int k, int l, int m, int n, int o)
 {
-  return i + j + k + l + m + n + o;
+    return i + j + k + l + m + n + o;
 }
 
 int h()
 {
-  return g2(1, 2, 3, 4, 5, 6, 7);
+    return g2(1, 2, 3, 4, 5, 6, 7);
 }
